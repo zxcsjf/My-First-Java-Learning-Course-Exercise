c91a8d6f6d51a248bf23421252c8d75174e91d4b
Shanjifei <2572899924@qq.com>
2022-05-10 00:56:27 +0800

    Callable and Futures

--- src/com/Executors/ExecutorsDemo.java
+++ src/com/Executors/ExecutorsDemo.java
@@ -1,15 +1,42 @@
 package com.Executors;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
 public class ExecutorsDemo {
     public static void show() {
         var executor = Executors.newFixedThreadPool(10);
-        for (var i = 0; i < 20; i++)
-        executor.submit(() -> {
-            System.out.println(Thread.currentThread().getName());
-        });
+        var executor1 = Executors.newSingleThreadExecutor();
+        var executor2 = Executors.newScheduledThreadPool(4);
 
-        executor.shutdown();
+        System.out.println(executor.getClass().getName());
+        try {
+//            for (var i = 0; i < 20; i++)
+//                executor.submit(() -> {
+//                    System.out.println(Thread.currentThread().getName());
+//                });
+
+            var future = executor.submit(() -> {
+                LongTask.simulate();
+                return 1;
+            });
+
+            System.out.println("Do more work");
+            try {
+                var result = future.get();
+                System.out.println(result);
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        } finally {
+            // Wait completion of current task,
+            // but not accept new task.
+            executor.shutdown();
+
+            // force the existing task to stop.
+//        executor.shutdownNow();
+        }
+
+
     }
 }
