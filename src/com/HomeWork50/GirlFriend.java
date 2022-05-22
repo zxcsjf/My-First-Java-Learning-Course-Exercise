4d89c3128af45739b3c84dcd17f78d40ea8d5559
Shan Jifei <2572899924@qq.com>
2022-05-22 11:11:35 +0800

    GirlFriend !

---
+++ src/com/HomeWork50/GirlFriend.java
@@ -0,0 +1,21 @@
+package com.HomeWork50;
+
+public class GirlFriend {
+    private int age;
+    private int income;
+    private int loveCount;
+    private int relationProgress;
+
+    public GirlFriend(int age, int income, int loveCount, int relationProgress) {
+        this.age = age;
+        this.income = income;
+        this.loveCount = loveCount;
+        this.relationProgress = relationProgress;
+    }
+
+    public String canWeMarry(){
+        if (age <= 26 && income >= 6000 && loveCount <= 3 && relationProgress >= 90)
+            return "Yes!";
+        else return "No!";
+    }
+}
