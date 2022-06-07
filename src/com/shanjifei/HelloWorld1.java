decc14de54766b28d0327b9c2a82ca42080de203
zxcsjf <2572899924@qq.com>
2022-06-07 10:37:32 +0800

    2022-06-07 上午

--- src/com/shanjifei/HelloWorld1.java
+++ src/com/shanjifei/HelloWorld1.java
@@ -7,22 +7,32 @@ import java.util.Scanner;
 public class HelloWorld1 {
     public static void main(String[] args) {
 
-        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println(maxSubArray(nums));
-    }
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
 
-    private static int maxSubArray(int[] nums) {
-        int ans = nums[0];
-        int sum = 0;
-        for (int num : nums) {
-            if (sum > 0) {
-                sum += num;
-            } else {
-                sum = num;
-            }
-            ans = Math.max(ans, sum);
-        }
-        return ans;
+        System.out.println(num);
+        System.out.println(str1);
+        System.out.println(str2);
+
+        //     int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        //     System.out.println(maxSubArray(nums));
+        // }
+        //
+        // private static int maxSubArray(int[] nums) {
+        //     int ans = nums[0];
+        //     int sum = 0;
+        //     for (int num : nums) {
+        //         if (sum > 0) {
+        //             sum += num;
+        //         } else {
+        //
+        //             sum = num;
+        //         }
+        //         ans = Math.max(ans, sum);
+        //     }
+        //     return ans;
     }
 }
 
