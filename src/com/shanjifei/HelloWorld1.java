55a81c83c1a2e766146da78a31d7436db7c8c4aa
zxcsjf <2572899924@qq.com>
2022-06-07 09:39:49 +0800

    2022-06-06 试卷

--- src/com/shanjifei/HelloWorld1.java
+++ src/com/shanjifei/HelloWorld1.java
@@ -7,64 +7,24 @@ import java.util.Scanner;
 public class HelloWorld1 {
     public static void main(String[] args) {
 
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(nums));
+    }
 
-        // byte b = 1;
-        // short b0 = b;
-        // int b1 = b0;
-        // long b2 = b1;
-        //
-        // double d1 = b1;
-        // float f1 = b1;
-        // double d2 = f1;
-        //
-        // System.out.println(f1);
-        // char a = 'a';
-        // int a1 = a;
-        // System.out.println(a1 == a);
-        //
-        // int i1 = 11111111;
-        // long l2 = 213013789;
-        // int num = 1_111_111_111;
-        // float f1 = num;
-        // System.out.println(f1);
-        // double d1 = num;
-        // System.out.println(d1);
-
-        // short num2 = (short) num1;
-        // System.out.println(num2);
-        //
-        // char stu = 't';
-        // System.out.println(b1 + stu);
-
-        // System.out.println('a');
-        // System.out.println('a' + 1);
-        // System.out.println("hello" + 'a' + 1);
-        // System.out.println('a' + 1 + "hello");
-        // System.out.println("5 + 5 = " + 5 + 5);
-        // System.out.println(5 + 5 + " = 5 + 5");
-        // System.out.println(5 + 5.0);
-        // System.out.println(5 + "5" + 5.0);
-
-
-        // int x = 1;
-        // int y = 2;
-        // int z = 3;
-        // System.out.println(y += z-- / ++x);
-
-        // for (int i = 0; i < 6; i++) {
-        //     int k = ++i;
-        //     while (k < 5) {
-        //         System.out.print(i);
-        //         break;
-        //     }
-        // }
-        //
-        // int x = 3, y = 4;
-        // x = (y < x++) ? 1 : 0;
-        // System.out.print(x);
-
-        // }
-
+    private static int maxSubArray(int[] nums) {
+        int ans = nums[0];
+        int sum = 0;
+        for (int num : nums) {
+            if (sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            ans = Math.max(ans, sum);
+        }
+        return ans;
     }
 }
+
+
 
